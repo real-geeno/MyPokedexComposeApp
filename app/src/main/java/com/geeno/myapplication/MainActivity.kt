b67a7fcd40e2b713bbae9d412d35bff8769b977f
Domenico Cangelli <indigo0467@gmail.com>
2021-12-31 11:34:44 +0100

    set up navigation

--- app/src/main/java/com/geeno/myapplication/MainActivity.kt
+++ app/src/main/java/com/geeno/myapplication/MainActivity.kt
@@ -3,6 +3,13 @@ package com.geeno.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.geeno.myapplication.ui.theme.JetpackComposePokedexTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +17,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePokedexTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "pokemon_list_screen") {
+                    composable("pokemon_list_screen") {
 
+                    }
+                    composable(
+                        "pokemon_detail_screen/{dominantColor}/{pokemonName}",
+                        arguments = listOf(
+                            navArgument("dominantColor") {
+                                type = NavType.IntType
+                            },
+                            navArgument("pokemonName") {
+                                type = NavType.StringType
+                            })
+                    ) {
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) ?: Color.White }
+                        }
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+                    }
+                }
             }
         }
     }
